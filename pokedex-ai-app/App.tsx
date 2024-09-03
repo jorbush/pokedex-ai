5f877779fffdc8571d6e9f19a1720d0113d0e0ca
Jordi <55429631+jorbush@users.noreply.github.com>
2024-09-03 22:06:28 +0200

    init react native app

---
+++ pokedex-ai-app/App.tsx
@@ -0,0 +1,11 @@
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View className='flex-1 justify-center items-center bg-white'>
+      <Text>Open up App.tsx to start working on our app!</Text>
+      <StatusBar style='auto' />
+    </View>
+  );
+}
